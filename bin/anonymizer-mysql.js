2c2291ef7450b4a8fa428df98b390ec88983af2c
Javier Alonso <soyjan@yahoo.es>
2018-06-18 18:07:54 +0200

    Update with more files.

---
+++ bin/anonymizer-mysql.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const commander = require("commander");
+const { version } = require("../package.json");
+const { error } = console;
+const { exit, argv } = process;
+
+const anonymizer = require("../lib/anonymizer-mysql");
+
+if (typeof JSON.clone !== "function") {
+  JSON.clone = function(obj) {
+    return JSON.parse(JSON.stringify(obj));
+  };
+}
+
+commander.version(version).on("command:*", () => {
+  error(
+    `Invalid command: ${commander.args.join(" ")}\nSee --help for a list of available commands.`
+  );
+  exit(1);
+});
+
+commander
+  .option("-f, --force", "Overwrite the clone db if it already exists.")
+  .option("-c, --conf <file>", "Config file. Default: config.json")
+  .option("-d, --dump <file>", "Path to mysqldump. Default: system mysqldump.")
+  .option("-m, --mysql <file>", "Path to mysql. Default: system mysql.");
+
+commander.parse(argv);
+
+var conf_fileValue = commander.conf || __dirname + "/config.json";
+
+anonymizer.run(conf_fileValue, commander);
